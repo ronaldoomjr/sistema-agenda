00960dd9f381072658e85e5351f5a6b639ba0685
ronaldoomjr <ronaldoomjunior@gmail.com>
2024-07-29 09:58:10 -0300

    criação das rotas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,16 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+
+$routes->get('/', 'Usuarios::login');
+$routes->post('usuarios/login', 'Usuarios::login');
+$routes->get('usuarios/cadastro', 'Usuarios::cadastro');
+$routes->post('usuarios/cadastro', 'Usuarios::cadastro');
+$routes->get('usuarios/logout', 'Usuarios::logout');
+
+$routes->get('atividades', 'Atividades::index', ['filter' => 'auth']);
+$routes->get('atividades/create', 'Atividades::create', ['filter' => 'auth']);
+$routes->post('atividades/create', 'Atividades::create', ['filter' => 'auth']);
+$routes->get('atividades/edit/(:num)', 'Atividades::edit/$1', ['filter' => 'auth']);
+$routes->post('atividades/edit/(:num)', 'Atividades::edit/$1', ['filter' => 'auth']);
+$routes->get('atividades/delete/(:num)', 'Atividades::delete/$1', ['filter' => 'auth']);
